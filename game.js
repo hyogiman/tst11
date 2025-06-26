ec15ad8c249b19b573059f51b1ee45caf221b61a
hyogiman <77236186+hyogiman@users.noreply.github.com>
2025-06-27 00:11:44 +0900

    Update game.js

--- game.js
+++ game.js
@@ -579,7 +579,7 @@ async function processSecretCode(targetPlayer, targetPlayerId) {
 }result.type = 'clue';
                     result.title = '동료 탐정 정보';
                     result.content = '동료 탐정과 정보를 공유했습니다.';
-                }
+                
                 break;
 
             case 'criminal':
